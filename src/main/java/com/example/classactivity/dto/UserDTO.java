d2b0c53e91b61d5040a2df860469a82ca5c2bcf5
Highmaster <nzechideraann@gmail.com>
2023-04-27 19:54:20 +0100

    first commit

---
+++ src/main/java/com/example/classactivity/dto/UserDTO.java
@@ -0,0 +1,6 @@
+package com.example.classactivity.dto;
+
+public class UserDTO {
+    private String username;
+    private String password;
+}
---
+++ src/main/java/com/example/classactivity/user/User.java
@@ -0,0 +1,19 @@
+package com.example.classactivity.user;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+
+    private String password;
+    private String email;
+}
